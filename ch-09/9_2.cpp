eff1b2687de910757fc3e00119ba49e75eb6d443
Gabriel Bevilacqua Soares <ga.bevsoar@gmail.com>
2023-02-18 13:50:41 -0500

    Exercise 9.2

--- ch-09/9_2.cpp
+++ ch-09/9_2.cpp
@@ -1,7 +1,26 @@
 #include <cstdio>
+#include <cstring>
+#include <map>
 
 int main(int argc, char* argv[])
 {
+    std::map<size_t, int> len_map{};
+    for(int x{1}; x < argc; x++)
+    {
+        char* input = argv[x];
+        size_t input_len = strlen(input);
+
+        printf("Input string (%ld): '%s'\n", input_len, input);
+        len_map[input_len]++;
+    }
+
+    for(auto el : len_map)
+    {
+        printf("%3ld: ", el.first);
+        int x = el.second;
+        while(x--) printf("*");
+        printf("\n");
+    }
 
     return 0;
 }
